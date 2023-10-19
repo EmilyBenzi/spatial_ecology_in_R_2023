ab639a45519392c75a25c713c62e6a67c32f453e
EmilyBenzi <147046518+EmilyBenzi@users.noreply.github.com>
2023-10-19 17:23:24 +0200

    Create 03.communities.overlap.R

---
+++ 03.communities.overlap.R
@@ -0,0 +1,7 @@
+#relation among species in time
+library(overlap)
+# data from Kerinci-Seblat National Park in Sumatra, Indonesia (Ridout and Linkie, 2009)
+data(kerinci)
+head(kerinci)
+# The unit of time is the day, so values range from 0 to 1. 
+summary(kerinci)
