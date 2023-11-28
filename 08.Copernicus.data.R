4e883a06529682485c808f30a21cbe1233ed50ae
EmilyBenzi <147046518+EmilyBenzi@users.noreply.github.com>
2023-11-28 17:24:23 +0100

    Create 08.Copernicus.data.R

---
+++ 08.Copernicus.data.R
@@ -0,0 +1,10 @@
+##Copernicus datas
+
+#https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+
+library(ncdf4)
+library(terra)
+#set the working directory
+setwd("/Users/emilybenzi/Desktop/spatial ecology")
+
+#import the datas
