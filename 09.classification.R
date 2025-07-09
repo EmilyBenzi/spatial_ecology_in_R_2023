ab35989c3ff4cac2bab7e3dea33023ba4562a2da
EmilyBenzi <147046518+EmilyBenzi@users.noreply.github.com>
2025-07-09 12:03:26 +0200

    Update 09.classification.R

--- 09.classification.R
+++ 09.classification.R
@@ -1,89 +1,89 @@
 ##classifying satellite images and estimate the amount of change
 
-#passare da immagine originale a classi (land use classes)
-#partendo da una immagine satellitare
-#considerando 2 bands (red  in x and IR in y)
-#prendendo un qualsiasi pixel (es. corrispondente a vegetaazione, che sappiamom riflette molto IR avrè basso x e alto y)
-#considerando vari punti=pixel nell'immagine, questi si posizioneranno, in funzione della loro riflettanza, nel grafico delle bands'
-#questi pixel sono chiamati training site
-#che "spiegano" al sistema come riconosscere  cluster insieme di oggetti con stesse caraatteristiche 
-#in esempio di img. abbiamo cluster agricoltura, foresta, acqua
-#dopo aver creato questi cluster, prendendo un qualsiasi nuovo punto nell'immagine è possibile assegnargli una classe in funzione della distanza minima del punto ad una certa calsse (sarà piu probabile che questo punto, con la sua riflettanza corrisponde alla classe piu vicina)
-#per fare qeusto in imagery è a disposizione la funzione "im."
+#going from original image to classes (land use classes)
+#starting from a satellite image
+#considering 2 bands (red in x and IR in y)
+#taking any pixel (e.g., corresponding to vegetation, which we know reflects a lot of IR; it will have low x and high y)
+#considering various points = pixels in the image, these will be positioned, depending on their reflectance, in the band graph.
+#these pixels are called training sites
+#which "teach" the system how to recognize clusters of objects with the same characteristics.
+#in example image. We have clusters of agriculture, forest, and water.
+#After creating these clusters, by taking any new point in the image, it is possible to assign it a class based on the minimum distance from the point to a certain class (it will be more likely that this point, with its reflectance, corresponds to the closest class).
+#To do this, the function "im.classify("img name", num_classrs="n") is available in Imagery.
 
-
+install.packages("ggplot2")
 library(imageRy)
 library(terra)
+library(ggplot2)
+install.packages("patchwork")
+library(patchwork)
 
 im.list()
-#c'è una immainge "solar_orbiter...", su questo si puo valutare la quantità di energia del sole ad esempio 
-#(puoi cercare quel paramtro su internet)
-#in funzione di questi colori sono associati diversi valori di energia + alto=giallo medio=marrone -=nero
+#There's a "solar_orbiter..." image, which can be used to evaluate the amount of energy from the sun, for example.
+#(You can search for that parameter online.)
+#These colors are associated with different energy values. + High = yellow, medium = brown, - = black.
 
-#ora classifichiamo questa immagine, spieghiamo al software quali sono i cluster (ce ne aspettiamo 3)
-
+#Now let's classify this image and tell the software which clusters there are (we expect 3).
 
 
 sun <- im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 
-sunc <- im.classify(sun, num_clusters=3) #le immagini verrano simili, ma non ugualii anche con stessa img, computer puo usare diverse random classification
+sunc <- im.classify(sun, num_clusters=3) #images will be similar, but not the same even with the same img, computer can use different random classifications
 
 plot(sunc)
 
-#per capire a quale livello di energia corrispondono le classi create si devono comparare ad immagine originale 
-#classe 1 = minima energia 
-#classe 2 = alta energia 
-#classe 3 = media energia
+#To understand the energy level of the created classes, you must compare them to the original image.
+#class 1 = minimum energy
+#class 2 = high energy
+#class 3 = medium energy
 
-##classify satellite data
-#usaimo ora delle immgaini di matogrosso
+#classify satellite data
+#use matogrosso images now
 
 im.list()
-m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg") #waning massage è soo perchè non ci sono le coordinate dell'immagine 
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg") #waning massage is just because there are no image coordinates
 m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
-#ricorda ross=vegetazione, l'acqua non è nera perche ci sono mlti solidi disciolti quidni viene come la terra 
+#remember ross=vegetation, the water isn't black because there are a lot of dissolved solids, so it looks like earth.
 
-#nella immagine del 1992 potrebbe essere complicato distingure le classi perchè l'immagine è molto frammentata
+#in the 1992 image, it might be difficult to distinguish the classes because the image is very fragmented.
 
-m1992c <- im.classify(m1992, num_clusters = 2) #non abbiamo messo la classe dell'acqua, che risulta come terra
+m1992c <- im.classify(m1992, num_clusters = 2) #we didn't put the water class, which appears as earth
 
 plot(m1992c)
-#classe 1 vegetazione 
-#classe 2 terra/agricoltura/intervento umano
-#computer usa rondom pixel per partire con classificazione 
+#Class 2 Vegetation (Green)
+#Class 1 Land/Agriculture/Human Intervention (White)
+#Computer uses round pixels to start with classification
 
-
-#lassifichiamo la seconda immagine
+#let's classify the second image
 m2006c <- im.classify(m2006, num_clusters = 2)
 plot(m2006c)
-#in questo caso classe 1= vegetazione (bianco), classe 2 =terra (verde)
+#in this case class 1 = vegetation (white), class 2 = earth (green)
 
 par(mfrow=c(1,2))
 plot(m1992c[[1]])
 plot(m2006c[[1]])
-#abbiamo selezionato la prima immagine per poterle confrontare, in questa ersione del programma ne plotta 3, ma sono tutte uguali
-#riscaricare imagery
+#We selected the first image to compare them. This version of the program plots three, but they're all the same.
+#Redownload Imagery
 
-#voglimo capire ora qual'è la proporzione di ogni classe
-#c'è una unzione "freq()" (calcola la frequenza dei pixel di una certa classe)
+#Now we want to understand the proportion of each class.
+#There's a "freq()" function (calculates the frequency of pixels in a given class).
 f1992 <- freq(m1992c)
 f1992
-#avendo 3 mappe si ripetono 3 volte 
-#comunque in ogni caso "count" indica il numero di pixel della classe corrispondente
-#possimao quindi calcolare la percentuale di una certa classe
-#troviamo il totale di pixel dell'immagine con la funzione ncell (funziona in ogni immagine)
+#having 3 maps, they are repeated 3 times
+#in any case, "count" indicates the number of pixels in the corresponding class
+#we can therefore calculate the percentage of a certain class
+#we find the total number of pixels in the image with the ncell function (it works on every image)
 
 tot1992 <- ncell(m1992c)
 tot1992
 
-#caloliamo la percentuale
+#let'scalculatethepercentage
 p1992 <- f1992 * 100 / tot1992
 p1992
-#i valori corripspondenti alla % sono quelli in "count"
-#abbiamo che classe 1 = terra=16.9%
-#classe 2 =foresta =83%
-
-#facciamo lo stesso per l'immagine del 2006
+#the values corresponding to the % are those in "count"
+#we have that class 1 = land = 16.9%
+#class 2 = forest = 83%
+#let's do the same for the 2006 image
 f2006 <- freq(m2006c)
 f2006
 tot2006 <- ncell(m2006c)
@@ -91,41 +91,41 @@ tot2006
 
 p2006 <- f2006 * 100 / tot2006
 p2006
-#abbiamo che classe 1 = foresta=45.3%
-#classe 2 =terra =54.69%
-#"layer" indica la mapa (ne abbiamo 1 infatti)
-#anche in questo caso le % possono essere un po diverse in funzione del computer
+#we have that class 1 = forest = 45.3%
+#class 2 = land = 54.69%
+#"layer" indicates the map (we actually have 1)
+#even in this case the %s may vary slightly depending on the computer
 
-#facciamo ora una tabella con questi dati
-#"data.frame()" permette di formare una tabella dove nelle riche abbiamo le classi e nelle colonne le due immagini, così da conforntare le percentuali di ogni classe a confronto
+#let's now create a table with this data
+#"data.frame()" allows you to create a table where the rows contain the classes and the columns contain the two images, so you can compare the percentages for each class.
 
-#building the fiinal table
+#building the final table
 class <- c("forest", "human")
-y1992 <- c(83, 17) #i numeri sono le percentuali del 1992
+y1992 <- c(83, 17) #the numbers are percentages from 1992
 y2006 <- c(45, 55)
-#così abbiamo definito le colonne
-
+#this is how we defined the columns
 
 tab <- data.frame(class, y1992, y2006) #come statistica
 tab
-#prima tanta foresta, poi quasi uguali
+#first a lot of forest, then almost the same
 
-#ora plottiamo il grafico, le funzioni per fare cio sono nel pack "ggplot"
+#now let's plot the graph; the functions for this are in the "ggplot" pack
 p1 <- ggplot(tab, aes(x=class, y=y1992, color=class)) + geom_bar(stat="identity", fill="white")
 p1
-#ottenisamo il grafico delle percentuali nel 1992
+#we get the percentage graph in 1992
 p2 <- ggplot(tab, aes(x=class, y=y2006, color=class)) + geom_bar(stat="identity", fill="white")
 p2
-#percetnuale delle componeneti nel 2006
+#percentage of components in 2006
 
-#ora possiamom unirli in modo da confrontarli al meglio
-#sfurttiamo il pack patch... (cercare funzioni su internet)
+#now we can combine them so we can better compare them
+#let's use the patch pack... (search for features on the internet)
 
 p1+p2
-#ora liabbiamo insieme, ma le scale sono diverse, non è chiaro l'ammount del problema
+#Now we have them together, but the scales are different, the magnitude of the problem is unclear.
 
-#dobbiamo metterelo stesso limite alla y, che vogliamo essere uguale al 100 perchè sono %
-#funzione ylim()
+#We need to set the same limit on the y-value, which we want to be equal to 100 because it's %
+#ylim() function
 p1 <- ggplot(tab, aes(x=class, y=y1992, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p2 <- ggplot(tab, aes(x=class, y=y2006, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p1 + p2
+
